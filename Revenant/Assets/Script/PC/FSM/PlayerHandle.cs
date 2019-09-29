e9e0c9e5f25e06ea460ef9d514e56a319a326011
박상훈 <tkdgnszhekr@gmail.com>
2019-09-30 04:05:24 +0900

    퍼즐작업 0.1v

--- Revenant/Assets/Script/PC/FSM/PlayerHandle.cs
+++ Revenant/Assets/Script/PC/FSM/PlayerHandle.cs
@@ -2,39 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerIdleState : PlayerState
+public class PlayerHandle : PlayerState
 {
     private Player player;
+    Vector3 basePos;
     void PlayerState.OnEnter(Player player)
     {
         //player 프로퍼티 초기화
         this.player = player;
+        basePos = player.transform.localPosition;
+        player.MoveCalc(0);
     }
     void PlayerState.Update()
     {
+        player.HandleRotation();
         if (player.cc.isGrounded)
         {
             player.yVelocity = 0;
         }
-        if (player.nowSpeed != 0)
-        {
-            player.SetState(new PlayerMoveState());
-            player.transform.GetChild(0).GetComponent<Animator>().SetBool("move", true);
-        }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            player.UseHandle();
-            Debug.Log("IdleE");
+            player.transform.parent = null;
+            player.SetState(new PlayerIdleState());
+            player.transform.GetChild(0).GetComponent<Animator>().SetBool("move", false);
+            Debug.Log("HandleE");
         }
-        player.MoveCalc(1f);
-        player.Gravity();
     }
     void PlayerState.OnExit()
     {
         //종료되면서 정리해야할것 구현
     }
-    
-    
-
 }
-
