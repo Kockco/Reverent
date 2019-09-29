776009e97304b2d6ea9ac74ad77b4cd6f2252911
박상훈 <tkdgnszhekr@gmail.com>
2019-09-29 22:14:17 +0900

    9.29 대격변 패치 (이전 스크립트 전부 지움)

--- Revenant/Assets/Script/NewGame/PlayerFSM.cs
+++ Revenant/Assets/Script/NewGame/PlayerFSM.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class PlayerFSM : MonoBehaviour
 {
     Rigidbody rb;
+
+    float h, v, r;
+    // 회전 속도 변수
+    public float rotSpeed = 80.0f;
+
+    public Transform model;
+    //이동관련
+    Vector3 move;
+    Transform myTransform;
+    [Range(0.1f, 30.0f)]
+    public float moveSpeed = 5;
+    [Range(0.1f, 30.0f)]
+    public float jumpPower = 6f;
+
+    public float mySpeed = 4;
+
+    public Transform cameraTransform;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+
+        model = transform.GetChild(0);
+        cameraTransform = Camera.main.transform.parent;
+        myTransform = transform;
     }
     private void Update()
     {
-        Vector3 inputMoveXZ = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Debug.Log(rb.velocity);
-        if(rb.velocity.x < 7.5f && rb.velocity.x > -7.5f)
-            rb.AddForce(new Vector3(inputMoveXZ.normalized.x * 25,0,0));
-        if (rb.velocity.z < 7.5f && rb.velocity.z > -7.5f)
-            rb.AddForce(new Vector3(0, 0, inputMoveXZ.normalized.z * 25));
-        //rb.velocity = inputMoveXZ.normalized * 6;
+        //GetAxis 0~1 GetAxisRaw 0-1
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        r = Input.GetAxisRaw("Mouse X");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        myTransform.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        // Vector3.up 축을 기준으로 rotSpeed만큼의 속도로 회전
+        myTransform.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+
     }
+
+    public void MoveCalc(float ratio)
+    {
+    }
+
 }
