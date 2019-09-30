8ef074275d699fba56e1e2cc0c471bc10b84f81a
박상훈 <tkdgnszhekr@gmail.com>
2019-10-01 03:04:25 +0900

    퍼즐 v 0.2

---
+++ Revenant/Assets/Script/Sanghoon/Puzzle/PuzzleHandle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleHandle : MonoBehaviour
+{
+    public PuzzlePlate link_plate;
+    bool isCatch;
+}
---
+++ Revenant/Assets/Script/Sanghoon/Puzzle/PuzzlePlate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePlate : MonoBehaviour
+{
+    [Header("퍼즐 링크")]
+    public PuzzleHandle link_handle;
+    [Header("붙어있는 감자 갯수")]
+    public int potatoCount;
+    //멈추는 지점
+    public float[] stopAngle;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //멈추는 지점
+        stopAngle = new float[potatoCount];
+
+        //360 / ? 로 각도 정하기
+        float angle = 360;
+        if (potatoCount != 0)
+            angle /= potatoCount;
+
+        //현재 y로테이션 값에서  360 나눠 구하기
+        float startAngle = transform.eulerAngles.y;
+        for(int i = 0; i < potatoCount; i++)
+        {
+            if((angle * i) + startAngle < 360)
+            stopAngle[i] = (angle * i)+ startAngle;
+            else
+            {
+                stopAngle[i] = (angle * i) + startAngle - 360;
+            }
+        }
+
+        if (link_handle == null)
+        {
+            Debug.Log("not link handle");
+        }
+        else
+        {
+            link_handle.link_plate = this;
+        }
+    }
+}
