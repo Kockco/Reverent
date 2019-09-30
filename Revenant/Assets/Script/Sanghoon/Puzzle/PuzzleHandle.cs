9a323aa6129382ae0d986c2cd78267a8f5c98a1f
박상훈 <tkdgnszhekr@gmail.com>
2019-10-01 03:35:58 +0900

    퍼즐 작업중

--- Revenant/Assets/Script/Sanghoon/Puzzle/PuzzleHandle.cs
+++ Revenant/Assets/Script/Sanghoon/Puzzle/PuzzleHandle.cs
@@ -5,5 +5,23 @@ using UnityEngine;
 public class PuzzleHandle : MonoBehaviour
 {
     public PuzzlePlate link_plate;
-    bool isCatch;
+    public bool isCatch;
+
+    private void Start()
+    {
+        transform.rotation = link_plate.transform.rotation;
+    }
+    private void Update()
+    {
+        
+    }
+    public void CatchCheck()
+    {
+        if (isCatch == false)
+        {
+            isCatch = true;
+        }
+        else
+            isCatch = false;
+    }
 }
