f1abb86d42be08190a8e1012f3ddc4a779c7720f
박상훈 <tkdgnszhekr@gmail.com>
2019-10-08 03:03:51 +0900

    퍼즐3 v0.9

---
+++ Revenant/Assets/Script/Sanghoon/PlanetPuzzle/PlanetRevolution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRevolution : MonoBehaviour
+{
+    //그냥 항상 부모의 한칸에 반을 도는 녀석
+    PlanetLine myPlanet;
+    public GameObject revolutionPlanet;
+    float rotateSpeed;
+    
+    private void Start()
+    {
+        myPlanet = GetComponent<PlanetLine>();
+    }
+    private void Update()
+    {
+        revolutionPlanet.transform.rotation = Quaternion.Euler(0,(transform.eulerAngles.y * myPlanet.cutAngle/2)+ 90, 0);
+    }
+    
+}
--- Revenant/Assets/Script/Sanghoon/PlanetPuzzle/PlanetSecondLine.cs
+++ Revenant/Assets/Script/Sanghoon/PlanetPuzzle/PlanetSecondLine.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetLine : MonoBehaviour
+public class PlanetSecondLine : MonoBehaviour
 {
+    PlanetCollision planetCol;
+    //자식으로 있냐없냐 판단
+    public bool isChild;
+
     public GameObject planet;
 
     //핸들과 연결
@@ -20,8 +24,14 @@ public class PlanetLine : MonoBehaviour
     //현재 멈춰있는지 알기위함
     public bool isLock;
 
+    public int firstChangePoint;
+    public int secondChangePoint;
+
     void Awake()
     {
+        planetCol = planet.GetComponent<PlanetCollision>();
+
+        isChild = true;
         //멈추는 지점
         stopAngle = new float[cutAngle];
         //중앙지점(왼쪽으로갈건지 오른쪽으로 갈건지 결정)
@@ -41,20 +51,20 @@ public class PlanetLine : MonoBehaviour
 
     void Update()
     {
-          AutioRotation();
+        if (isChild)
+            AutioRotation();
     }
-
 
     public void PlanetRotate(float direction)
     {
-        transform.Rotate(0,rotateSpeed * direction * Time.deltaTime,0);
+        transform.Rotate(0, rotateSpeed * direction * Time.deltaTime, 0);
         ChildRotation(direction);
     }
 
     //왼쪽으로 맟출건지 오른쪽으로 맟출건지 결정
     public void AngleCheck()
     {
-        for(int i = 0; i <cutAngle; i++)
+        for (int i = 0; i < cutAngle; i++)
         {
             if (i == cutAngle - 1)
             {
@@ -65,7 +75,7 @@ public class PlanetLine : MonoBehaviour
             }
             else
             {
-                if (transform.eulerAngles.y > centerAngle[i] && transform.eulerAngles.y < centerAngle[i+1])
+                if (transform.eulerAngles.y > centerAngle[i] && transform.eulerAngles.y < centerAngle[i + 1])
                 {
                     myPoint = i + 1;
                 }
@@ -76,7 +86,7 @@ public class PlanetLine : MonoBehaviour
     //자전
     void ChildRotation(float direction)
     {
-        planet.transform.Rotate(0, rotateSpeed *direction * Time.deltaTime, 0);
+        planet.transform.Rotate(0, rotateSpeed * direction * Time.deltaTime, 0);
     }
 
     //자동으로 움직이기
@@ -93,6 +103,23 @@ public class PlanetLine : MonoBehaviour
                     isLock = true;
                 }
             }
+        }
+    }
+
+    public void LineCheck()
+    {
+        if(planetCol.onChange )
+        {
+            if(planetCol.number ==1)
+            {
+                myPoint = firstChangePoint;
+            }
+            else if (planetCol.number == 2)
+            {
+                myPoint = secondChangePoint;
+            }
+
+            planet.transform.SetParent(transform);
         }
     }
 }
