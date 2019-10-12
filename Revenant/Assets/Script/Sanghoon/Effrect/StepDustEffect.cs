96b106d320d8bd7d725eb2550fa5611b310da027
박상훈 <tkdgnszhekr@gmail.com>
2019-10-12 15:09:59 +0900

    퍼즐3

---
+++ Revenant/Assets/Script/Sanghoon/Effrect/StepDustEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepDustEffect : MonoBehaviour
+{
+    public GameObject stepDust;
+    public float y;
+    public float dustSpeed;
+    
+    void Update()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + y, transform.position.z);
+        stepDust.transform.position =Vector3.MoveTowards(stepDust.transform.position, pos, dustSpeed * Time.deltaTime);
+    }
+}
