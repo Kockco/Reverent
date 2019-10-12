b6e0429f66157599c21dec911b5f4954c4172fd0
박상훈 <tkdgnszhekr@gmail.com>
2019-10-12 20:39:00 +0900

    ,,

---
+++ Revenant/Assets/Script/Sanghoon/PuzzleManger/PuzzleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public StarPlate[] starPuzzle1;
+    public StarPlate[] starPuzzle2;
+
+    public ParticleSystem[] particle;
+
+    void Update()
+    {
+    }
+
+    public void StarPuzzleClearCheck()
+    {
+        foreach(StarPlate starPuzzle in starPuzzle1)
+        {
+            if (starPuzzle.isLock == true &&
+                starPuzzle.myPoint == 0)
+            {
+
+            }
+        }
+    }
+}
