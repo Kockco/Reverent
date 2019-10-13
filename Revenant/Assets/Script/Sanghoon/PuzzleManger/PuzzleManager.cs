4b20ca72c439c0266c184b485d3f5d390e8597da
박상훈 <tkdgnszhekr@gmail.com>
2019-10-14 03:03:41 +0900

    퍼즐 매니저

--- Revenant/Assets/Script/Sanghoon/PuzzleManger/PuzzleManager.cs
+++ Revenant/Assets/Script/Sanghoon/PuzzleManger/PuzzleManager.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PuzzleManager : MonoBehaviour
 {
     public StarPlate[] starPuzzle1;
-    public GameObject starPuzzle1Pos;
     public StarPlate[] starPuzzle2;
-    public GameObject starPuzzle2Pos;
+    public StarPlate[] potatoPuzzle1;
+    public StarPlate[] potatoPuzzle2;
+    public StarPlate[] planetPuzzle1;
 
     public ParticleSystem[] starPuzzleParticle;
+    public ParticleSystem[] starPuzzleParticle2;
+    public ParticleSystem[] potatoPuzzleParticle;
+    public ParticleSystem[] potatoPuzzleParticle2;
+    public ParticleSystem[] planetPuzzleParticle;
 
     void Update()
     {
@@ -21,12 +26,32 @@ public class PuzzleManager : MonoBehaviour
         {
             if (starPuzzle.myPoint == 0)
             {
-                starPuzzleParticle[0].transform.position = starPuzzle1Pos.transform.position;
                 foreach (ParticleSystem effect in starPuzzleParticle)
                 {
                     effect.Play();
                 }
             }
+        }
+    }
+    public void PuzzleClearCheck(int puzzleNumber)
+    {
+        int clearPoint = 0;
+        switch (puzzleNumber)
+        {
+            case 1:
+                foreach (StarPlate starPuzzle in starPuzzle1)
+                {
+                    if (starPuzzle.myPoint == 0)
+                    {
+                        clearPoint++;
+                    }
+                    if (starPuzzle1.Length == clearPoint)
+                    {
+
+                    }
+                }
+
+                break;
         }
     }
 }
