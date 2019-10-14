69561a26298227cd26b8bd05fe8c8cd95e2c95ae
tankmon8 <tankmon8@naver.com>
2019-10-15 02:07:47 +0900

    - Cam Wall Crush Fix

--- Revenant/Assets/Script/Sungmin/MomiFSM/Momi_Handle.cs
+++ Revenant/Assets/Script/Sungmin/MomiFSM/Momi_Handle.cs
@@ -143,8 +143,8 @@ public class Momi_Handle : MomiFSMState
     {
         GameObject[] handleLeftRight;
         handleLeftRight = new GameObject[2];
-        handleLeftRight[0] = transform.parent.GetChild(1).gameObject;
-        handleLeftRight[1] = transform.parent.GetChild(2).gameObject;
+        handleLeftRight[0] = transform.parent.GetChild(0).gameObject;
+        handleLeftRight[1] = transform.parent.GetChild(1).gameObject;
 
         if (Vector3.Distance(transform.position, handleLeftRight[0].transform.position) <
             Vector3.Distance(transform.position, handleLeftRight[1].transform.position))
